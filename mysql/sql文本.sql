ec648180682692d5043b9a3927b2f2d15b994dbe
XT <1804352454@qq.com>
2017-08-06 20:16:12 +0800

    修改phone为password

--- mysql/sql文本.sql
+++ mysql/sql文本.sql
@@ -2,6 +2,6 @@
 CREATE TABLE addresslist (  
     id INT NOT NULL AUTO_INCREMENT,  
     name VARCHAR(40) NOT NULL,  
-    phone VARCHAR(40) NOT NULL,  
+    password VARCHAR(40) NOT NULL,  
     PRIMARY KEY ( id )  
 );  
